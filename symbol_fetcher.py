b46ee8db64ab8e7d91704f83f413a5b60d76e4f3
b0a04gl <balaji.foo404@gmail.com>
2023-10-30 14:15:19 +0530

    feature : fetch S&P 500 scripts

---
+++ symbol_fetcher.py
@@ -0,0 +1,19 @@
+import csv
+import pandas as pd
+
+
+def write_symbols():
+    sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+
+    sp500['Symbol'] = sp500['Symbol'].str.replace('.', '-')
+
+    symbols_list = sp500['Symbol'].unique().tolist()
+
+    with open('symbols.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(symbols_list)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    write_symbols()
